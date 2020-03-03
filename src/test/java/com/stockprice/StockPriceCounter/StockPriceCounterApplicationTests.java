2fa6f938df17a79c63d03c65c76a0e1e48c42266
Wilson Saripalli <sowmyawilson@Wilsons-MacBook-Pro.local>
2020-03-02 23:08:39 -0500

    StockPriceCounter init

---
+++ src/test/java/com/stockprice/StockPriceCounter/StockPriceCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockprice.StockPriceCounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPriceCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
